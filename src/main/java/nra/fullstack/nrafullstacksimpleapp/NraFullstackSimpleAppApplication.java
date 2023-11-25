0cd87a75d25b553187535b490ad540169cb97a65
nra <nick.a_login@yahoo.com>
2023-11-25 13:48:58 +0300

    test 2

---
+++ src/main/java/nra/fullstack/nrafullstacksimpleapp/NraFullstackSimpleAppApplication.java
@@ -0,0 +1,13 @@
+package nra.fullstack.nrafullstacksimpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NraFullstackSimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NraFullstackSimpleAppApplication.class, args);
+	}
+
+}
